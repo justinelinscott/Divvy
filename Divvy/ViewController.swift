cc194a485e5234bd8715ba18ffbad2506fdd97bb
Justine Linscott <justinelinscott@unomaha.edu>
2019-06-04 13:05:41 -0500

    Initial Commit

---
+++ Divvy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Divvy
+//
+//  Created by Justine Linscott on 6/4/19.
+//  Copyright © 2019 Justine Linscott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
